8ff2656a16ab27f10a0f19a38ad49bd4e369264d
Maxim <maxlukyrabbit@yandex.ru>
2024-02-16 20:36:20 +0300

    first commit

---
+++ console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DealManager.ChangeStage("43745", "C25:EXECUTING"));
+            Console.WriteLine(SearchDeal.SearchDealMethod("1944102691"));
+        }
+    }
+}
